74d59564230b3872ac7afd907d0f1fb9ec464320
Johel <49104482+JohelPF@users.noreply.github.com>
2022-05-11 19:06:50 -0600

    Clases Adicionales

---
+++ EstacionaTEC/Models/Reserva.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstacionaTEC.Models
+{
+    public class Reserva
+    {
+        private int identificacionPersona;
+        private string placa;
+        private TimeSpan horaEntrada;
+        private TimeSpan horaSalida;
+        private int idEspacio;
+
+        public int IdentificacionPersona { get => identificacionPersona; set => identificacionPersona = value; }
+        public string Placa { get => placa; set => placa = value; }
+        public TimeSpan HoraEntrada { get => horaEntrada; set => horaEntrada = value; }
+        public TimeSpan HoraSalida { get => horaSalida; set => horaSalida = value; }
+        public int IdEspacio { get => idEspacio; set => idEspacio = value; }
+    }
+}
