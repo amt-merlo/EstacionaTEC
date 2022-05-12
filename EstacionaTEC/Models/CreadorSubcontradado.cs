01d6761c6def4e5c280658406d998bb3e7633856
Johel <49104482+JohelPF@users.noreply.github.com>
2022-05-11 18:09:08 -0600

    Factory

---
+++ EstacionaTEC/Models/CreadorSubcontradado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstacionaTEC.Models
+{
+    public class CreadorSubcontradado : CreadorEstacionamiento
+    {
+        public override Estacionamiento createEstacionamiento()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EstacionaTEC/Models/EstacionamientoSubcontratado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstacionaTEC.Models
+{
+    public class EstacionamientoSubcontratado : Estacionamiento
+    {
+        private int IdEstacionamiento;
+        private string nombre;
+        private string ubicacion;
+        private int cantidadEspaciosTotal;
+        private int cantidadEspaciosActual;
+        private int cantidadVehicEspecial;
+        private TimeSpan horaApertura;
+        private TimeSpan horaCierre;
+        private string nombreContrato;
+        private int numeroTelefonoContacto;
+        private string contrato;
+
+        public int IdEstacionamiento1 { get => IdEstacionamiento; set => IdEstacionamiento = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Ubicacion { get => ubicacion; set => ubicacion = value; }
+        public int CantidadEspaciosTotal { get => cantidadEspaciosTotal; set => cantidadEspaciosTotal = value; }
+        public int CantidadEspaciosActual { get => cantidadEspaciosActual; set => cantidadEspaciosActual = value; }
+        public int CantidadVehicEspecial { get => cantidadVehicEspecial; set => cantidadVehicEspecial = value; }
+        public TimeSpan HoraApertura { get => horaApertura; set => horaApertura = value; }
+        public TimeSpan HoraCierre { get => horaCierre; set => horaCierre = value; }
+        public string NombreContrato { get => nombreContrato; set => nombreContrato = value; }
+        public int NumeroTelefonoContacto { get => numeroTelefonoContacto; set => numeroTelefonoContacto = value; }
+        public string Contrato { get => contrato; set => contrato = value; }
+
+        public void CrearEspacio()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
