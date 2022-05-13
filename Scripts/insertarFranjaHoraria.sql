efb148876d38e6b8e893840f1ab13294ef62a144
BrayanMQ <brayanmq11288@estudiantec.cr>
2022-05-13 16:14:03 -0600

    Sp para insertar franjas horarias

---
+++ Scripts/insertarFranjaHoraria.sql
@@ -0,0 +1,53 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Brayan Marín Quirós>
+-- Create date: <12/05/2022>
+-- Description:	<Sp para insertar franjas horarias>
+-- =============================================
+CREATE PROCEDURE [dbo].[insertarFranjaHoraria](@inIdDia INT,
+											   @inIdPersona INT,
+											   @inInicio TIME(7),
+											   @inFinal TIME(7)
+											  )
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    --Inserta una franja horaria si no existe
+	IF NOT EXISTS(SELECT * 
+				  FROM [dbo].[FranjaHoraria] AS FH
+				  WHERE @inIdDia = FH.idDia 
+						AND @inIdPersona = FH.idPersona 
+						AND (@inInicio = FH.inicio 
+							 OR @inFinal = FH.fin)
+				 )
+	BEGIN
+		INSERT INTO [dbo].[FranjaHoraria] ([idDia], [idPersona], [inicio], [fin])
+		VALUES (@inIdDia, @inIdPersona, @inInicio, @inFinal)
+
+		RETURN 1 --Sin error
+	END
+
+	ELSE
+
+	BEGIN
+		RETURN 0 --Error
+	END
+END
+GO
