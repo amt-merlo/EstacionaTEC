a37fea06074d2a1c0b06c50a2daae734aa7ffd6c
Johel <49104482+JohelPF@users.noreply.github.com>
2022-05-13 18:52:13 -0600

    Update GestorPersonas.cs

--- EstacionaTEC/Controllers/GestorPersonas.cs
+++ EstacionaTEC/Controllers/GestorPersonas.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using EstacionaTEC.Controllers;
+using EstacionaTEC.Controllers.DAO;
 using EstacionaTEC.Models;
 
 namespace EstacionaTEC.Controllers
 {
     public class GestorPersonas
     {
+        private readonly DAOPersonas dao = new DAOPersonas();
         public bool crearPersona(DTOPersonas dto)
         {
-
             Persona persona = new Persona(dto.Identificacion,dto.NombreCompleto,dto.NumTelefono,dto.CorreoInstitucional,dto.CorreoAlterno,dto.Departamento,dto.EsJefatura,dto.EsAdmin,dto.ServiciosEspeciales,dto.EsAdministrativo, dto.EstaEnPlanilla, dto.Contrasenna);
-            return true;
+            return dao.create(persona);
         }
     }
 }
